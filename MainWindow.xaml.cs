a59d9ac35d1129ca59bbd08575a6177d3801344b
doddy-s <doddy.s@protonmail.com>
2022-11-19 15:11:03 +0700

    responsive

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,6 +29,28 @@ namespace projectA
         {
             Close();
         }
+
+        private void MaxButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(WindowState == WindowState.Normal)
+                this.WindowState = WindowState.Maximized;
+            else
+                this.WindowState = WindowState.Normal;
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            // Begin dragging the window
+            this.DragMove();
+        }
+
+        private void MinButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
         /*
         Ellipse YourEllipseName = new Ellipse
         {
